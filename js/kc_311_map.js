b49cfc5ac638ffb51591877bd66cdb15b5b4586d
Andrew Hyder <hyd415@gmail.com>
2014-05-26 19:47:41 -0700

    Changed request name.

--- js/kc_311_map.js
+++ js/kc_311_map.js
@@ -7,7 +7,7 @@ var mapUrl = 'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.jpg',
     mapAttrib = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles By <a href="http://stamen.com">Stamen</a>',
     mapInfo = new L.TileLayer(mapUrl, {maxZoom: 18, attribution: mapAttrib});
 
-// set a default location for the map       
+// set a default location for the map
 var kansascity = new L.LatLng(39.101, -94.583); // geographical point (longitude and latitude)
 map.setView(kansascity, 11).addLayer(mapInfo);
 var open_cases_list = [];
@@ -23,7 +23,7 @@ function add_yesterdays_markers(open_or_closed){
   if (open_or_closed == 'closed_date'){
     var marker_color = marker_blue;
   }
-  
+
   var d = new Date();
   var month = d.getMonth()+1;
   var day = d.getDate() - 1;
@@ -31,7 +31,7 @@ function add_yesterdays_markers(open_or_closed){
       ((''+month).length<2 ? '0' : '') + month + '-' +
       ((''+day).length<2 ? '0' : '') + day;
   var yesterday = output+'T00:00:00'
-  var yesterdays_cases = $.getJSON("http://data.kcmo.org/resource/7at3-sxhp.json?$where="+open_or_closed+"='"+yesterday+"'" 
+  var yesterdays_cases = $.getJSON("http://data.kcmo.org/resource/7at3-sxhp.json?$where="+open_or_closed+"='"+yesterday+"'"
     , function(data){
       // console.log(data);
       // console.log(data.length);
@@ -46,7 +46,7 @@ function add_yesterdays_markers(open_or_closed){
         var latitude = data[i].address_with_geocode.latitude;
         var longitude = data[i].address_with_geocode.longitude;
         markerLocation = new L.LatLng(parseFloat(latitude), parseFloat(longitude));
-        var marker = new L.Marker(markerLocation, {icon: marker_color}).bindPopup(data[i].case_summary+', '+data[i].creation_date);
+        var marker = new L.Marker(markerLocation, {icon: marker_color}).bindPopup(data[i].request_type+', '+data[i].creation_date);
         open_cases_list.push(marker);
       }
       var open_cases_layer = new L.LayerGroup(open_cases_list);
